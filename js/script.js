9fb8aba778a6e4bc63f951ecf96af0ea7c1a0838
Marco <marcopa84@hotmail.it>
2020-02-03 14:13:28 +0100

    mounth change OK!

--- js/script.js
+++ js/script.js
@@ -1,38 +1,47 @@
 $(document).ready(function () {
-  var mounths = 0;
+  var mounth = 0;
 
-  var daysInMounth = moment("2018-"+(mounths+1), "YYYY-MM").daysInMonth();
-  console.log(daysInMounth);
+  getDays(mounth);
 
-  getDays(daysInMounth);
-
+  // mese successivo
   $('.next-mounth').on('click', function() {
-    mounths = mounths + 1;
-    if (mounths > 11) {
+    mounth = mounth + 1;
+    if (mounth > 11) {
       alert('Calendario 2019 non pronto!');
     }
     else {
-      var daysInMounth = moment("2018-"+(mounths+1), "YYYY-MM").daysInMonth();
-      console.log(mounths);
-      console.log(daysInMounth);
       $('.mounth-days').text('');
-      getDays(daysInMounth);
+      getDays(mounth);
     }
-
   });
-
+  // mese precedente
+  $('.prev-mounth').on('click', function() {
+    mounth = mounth - 1;
+    if (mounth < 0) {
+      alert('Calendario 2017 non selezionabile!');
+    }
+    else {
+      $('.mounth-days').text('');
+      getDays(mounth);
+    }
+  });
 
 });
 
 
 // creo i giorni in base alla variabile dei giorni del mese selezionato
-function getDays(dayMax) {
+function getDays(mounth) {
+  var mounthsName = ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'];
+
+  var daysInMounth = moment("2018-"+(mounth+1), "YYYY-MM").daysInMonth();
+  $('.mounth-name').text(mounthsName[mounth]);
+
   var day=0;
-  for (var i = 0; i < dayMax; i++) {
+  for (var i = 0; i < daysInMounth; i++) {
     var source = document.getElementById("entry-template").innerHTML;
     var template = Handlebars.compile(source);
     var context = {
-      day : (day=day+1) + ' ' + "prova",
+      day : (day=day+1) + ' ' + mounthsName[mounth],
     };
     var html = template(context);
     $('.mounth-days').append(html);

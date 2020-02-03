ad26527a78c13892c3bb2f21f2a8328ad50559b0
Marco <marcopa84@hotmail.it>
2020-02-03 15:16:35 +0100

    Exercise OK!!!

--- js/script.js
+++ js/script.js
@@ -29,21 +29,57 @@ $(document).ready(function () {
 });
 
 
-// creo i giorni in base alla variabile dei giorni del mese selezionato
+// creo i giorni in base alla variabile dei giorni del mese selezionato + scrivo mese
 function getDays(mounth) {
+  // lista nomi mesi
   var mounthsName = ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'];
-
+  // calcolo i giorni del mese
   var daysInMounth = moment("2018-"+(mounth+1), "YYYY-MM").daysInMonth();
   $('.mounth-name').text(mounthsName[mounth]);
-
+  // ciclo per i giorni e creo tanti <li> quanti sono i giorni
   var day=0;
+  var mounthAttr = mounth +1;
   for (var i = 0; i < daysInMounth; i++) {
     var source = document.getElementById("entry-template").innerHTML;
     var template = Handlebars.compile(source);
     var context = {
-      day : (day=day+1) + ' ' + mounthsName[mounth],
+      attr: '2018-'+addZero(mounthAttr)+'-'+addZero(day=day+1),
+      day : day + ' ' + mounthsName[mounth],
     };
     var html = template(context);
     $('.mounth-days').append(html);
+
   };
+
+  $.ajax({
+    url: "https://flynn.boolean.careers/exercises/api/holidays?year=2018&month="+mounth,
+    method: "GET",
+    success: function (data, stato) {
+      var arrayFest = data.response;
+      console.log(data.response);
+      for (var i = 0; i < arrayFest.length; i++) {
+        console.log(arrayFest[i].date);
+        $('li').each(function() {
+          var dayAttr = $(this).attr('data-date');
+          if (dayAttr == arrayFest[i].date) {
+            $(this).children('.event').append(arrayFest[i].name);
+            $(this).addClass('event-day');
+          }
+        });
+      }
+
+    },
+    error: function (richiesta, stato, errori) {
+      alert("E' avvenuto un errore. " + errore);
+    }
+  });
+
+};
+
+// funzione per uniformare data con doppia cifra
+function addZero(number) {
+  if(number < 10) {
+    number = '0' + number;
+  }
+  return number;
 }
